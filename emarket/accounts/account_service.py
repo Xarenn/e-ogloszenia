b8adaabc67eebb2dce26664893ecc0752d7f0216
4d4s001 <Adam.Weberit@gmail.com>
2019-05-11 14:05:08 +0200

    init

--- emarket/accounts/account_service.py
+++
@@ -1,5 +0,0 @@
-from accounts.models import Ad
-
-
-def get_ads_by_username(username):
-    return Ad.objects.filter(user__username__contains = username)
--- emarket/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    path('', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-
-]
