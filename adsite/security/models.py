422e2bf87d01e13ea09f6f282be139735706f0a9
Adamicov <Adam.Weberit@gmail.com>
2019-05-15 15:14:39 +0200

    new user model

--- adsite/security/models.py
+++ adsite/security/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from datetime import date
 
-# Create your models here.
+from .managers import UserManager
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=60, unique=True)
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. ''Unselect this instead of deleting accounts.'
+        ),
+    )
+    name = models.CharField(max_length=40)
+    entry_date = models.DateField(default=date.today())
+    phone = models.CharField(max_length=18)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = UserManager()
+
+
+   
