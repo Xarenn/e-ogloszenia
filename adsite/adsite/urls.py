f8ea4b165728483e1451d3e9565536df20bdb593
Adamicov <Adam.Weberit@gmail.com>
2019-05-17 13:36:17 +0000

    auth, auth views

--- adsite/adsite/urls.py
+++ adsite/adsite/urls.py
@@ -1,11 +1,16 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from pages import views
+from security.views import register_view
+
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('', views.home_view, name='index'),
-    path('login', views.login_view, name='login'),
-    path('register', views.register_view, name='register'),
+    path('', views.home_view),
+    path('home/', views.home_view, name='home'),
+    path('register/', register_view, name='register'),
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(template_name='auth/login.html')),
+    path('logout/', LogoutView.as_view())
 ]
--- adsite/security/register_form.py
+++ adsite/security/register_form.py
@@ -1,26 +1,12 @@
 from django import forms
-
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import User
 
-class RegisterForm(forms.ModelForm):
-    password1 = forms.CharField(label='password1', max_length=30, widget=forms.PasswordInput)
-    password2 = forms.CharField(label='password2', max_length=30, widget=forms.Textarea)
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(max_length=100)
 
     class Meta:
         model = User
-        fields = ('email',)
+        fields = ('email', 'password1', 'password2',)
     
-    def clean_password2(self):
-        password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Password don't match")
-        return password2
-
-    def save(self, commit=True):
-        user = super(RegisterForm, self).save(commit=False)
-        user.set_password(self.cleaned_data.get('password1'))
-        if commit:
-            user.save()
-        return user
