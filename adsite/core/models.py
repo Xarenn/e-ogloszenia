7171d4996399c1684d2b8f3b688be9b05719b946
Adamicov <Adam.Weberit@gmail.com>
2019-05-19 01:48:50 +0000

    AD model

--- adsite/core/models.py
+++ adsite/core/models.py
@@ -1,2 +1,16 @@
 from django.db import models
+from security.models import User
 
+class Ad(models.Model):
+    title = models.CharField(max_length=70)
+    description = models.TextField()
+    entry_date = models.DateTimeField(auto_now_add=True)
+    bump_date = models.DateTimeField(auto_now_add=True)
+    images = models.ImageField(upload_to='/images', blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+    
+    
